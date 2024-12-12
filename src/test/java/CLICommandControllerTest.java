87f43b4c086f3242808af09ca28ca587052dd2d5
Biszku <113379368+Biszku@users.noreply.github.com>
2024-12-12 18:37:00 +0100

    Delete src/test directory

--- src/test/java/CLICommandControllerTest.java
+++
@@ -1,9 +0,0 @@
-import com.TMDBCLITool.CLICommandController;
-import org.junit.jupiter.api.Test;
-
-public class CLICommandControllerTest {
-    @Test
-    public void testSetCommandSize() {
-        CLICommandController.setArgumentsSize((byte) 2);
-    }
-}
